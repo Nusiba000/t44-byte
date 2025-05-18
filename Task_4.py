247071b58e1f03fe4468deb251c5d21404f917a2
Nusiba <nusibaalnabhani@gmail.com>
2025-05-18 14:55:35 +0400

    added

---
+++ Task_4.py
@@ -0,0 +1,3 @@
+num_1 = input("Enter the 1st number")
+num_2 = input("Enter the snd number")
+opreation = input("Enter one of bitwise or",  "bitwise and", "bitwise xor", "generate random", "percentage", "square root")
