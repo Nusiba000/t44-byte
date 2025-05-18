657eeca5ab63f2623a5db89d9de81c08f0550e77
khadija829 <khadijazad313@gmail.com>
2025-05-18 15:34:59 +0400

    add funvtion

--- Task_4.py
+++ Task_4.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-num_1 = input("Enter the 1st number")
-num_2 = input("Enter the snd number")
-opreation = input("Enter one of bitwise or",  "bitwise and", "bitwise xor", "generate random", "percentage", "square root")
-=======
 num_1 = 0
 opreation = 0
 num_2 = 0
@@ -16,4 +11,7 @@ opreation = input("Enter one of bitwise or, bitwise and,bitwise xor,generate ran
 if opreation == "bitwise or":
     result = num_1 | num_2
     print(result)
->>>>>>> d1392dea4c7ba07f0b8b10f853d700c99295e860
+
+# Perform Bitwise AND operation
+result = num_1 & num_2
+print(f"The result of {num_1} & {num_2} is: {result}")
